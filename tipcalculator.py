630f5cc30cbb33c28312a47dda1121ad8a5df2a6
maghalashvili <dato.maghala@gmail.com>
2023-03-13 03:50:09 +0400

    tipcalculate.py

---
+++ tipcalculator.py
@@ -0,0 +1,13 @@
+
+    
+dollars=float(input(("How much was the meal?")))                #str to nums
+percent=float(input(("What parcent would you like to tip?")))
+tip =float(dollars*percent)/100
+
+
+print(f"Leave $,{tip:.2f}") #calculate 
+
+
+
+
+
